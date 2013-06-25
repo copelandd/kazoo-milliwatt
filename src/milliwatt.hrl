a6c2fac0275e010be0245d4bb620e89a33434a32
Peter Defebvre <peter@2600hz.com>
2013-06-25 01:49:37 +0000

    KAZOO-415: move milliwatt to the applications folder

---
+++ src/milliwatt.hrl
@@ -0,0 +1,13 @@
+-ifndef(MILLIWATT_HRL).
+
+%% Typical includes needed
+-include_lib("whistle/include/wh_amqp.hrl").
+-include_lib("whistle/include/wh_types.hrl").
+-include_lib("whistle/include/wh_log.hrl").
+-include_lib("whistle/include/wh_databases.hrl").
+
+-define(APP_NAME, <<"milliwatt">>).
+-define(APP_VERSION, <<"1.0.0">>).
+
+-define(MILLIWATT_HRL, true).
+-endif.
